a2def47c82565f960fd6fbb2559211a1d7d7abc4
kkerns20 <kurtkerns20@gmail.com>
2022-10-07 10:10:50 -0500

    updated main.py to read Frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+book_path = "books/frankenstein.txt"
+
+
+def main():
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(book_path) as f:
+        return f.read()
+
+
+main()
